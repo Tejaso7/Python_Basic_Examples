d2993074e4cd44364fdd07e8fca244a7b21710b0
Tejas Sawant <tejusawant302@gmail.com>
2023-10-11 18:05:25 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,9 +1,9 @@
 
 class Student:
-    def mukund(self):
+    def tejas(self):
         self.name = input("Enter name : ")
         self.age = input("Enter age: ")
 
 o = Student()
-o.mukund()
+o.tejas()
 print("My Name is", o.name, "and my roll number is", o.age)
